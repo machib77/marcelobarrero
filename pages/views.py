b2ccaebd057dc32c6b5b9945cd2322a3c7c8555c
Marcelo Barrero <marcelo.barrero@live.com>
2024-02-25 18:56:24 -0400

    about basics

--- pages/views.py
+++ pages/views.py
@@ -5,3 +5,11 @@ from django.views.generic import TemplateView
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = "pages/home.html"
+
+
+class AboutPageView(TemplateView):
+    template_name = "pages/about.html"
+
+
+class ContactPageView(TemplateView):
+    template_name = "pages/contact.html"
