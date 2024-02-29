040654601c7ea788aa6a57f904c129db5c749520
Marcelo Barrero <marcelo.barrero@live.com>
2024-02-29 19:57:16 -0400

    some style improvements - ready to start with first project

--- django_project/settings.py
+++ django_project/settings.py
@@ -137,4 +137,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = "marcelo.barrero@live.com"
 EMAIL_HOST_PASSWORD = "pav3m3ntch1l1$"
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
 # os.environ.get("GMAIL_APP_PASSWORD")  # Recommended to use environment variables
