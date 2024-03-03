1d015aa736e35ea96751cff8ed9286bcb53229ca
Marcelo Barrero <marcelo.barrero@live.com>
2024-03-03 10:29:44 -0400

    swap_manager: fixed url hyphens

--- swap_manager/urls.py
+++ swap_manager/urls.py
@@ -2,12 +2,12 @@ from django.urls import path
 from .views import HomePageView, ChartGeneratorView, DiscountChartView, DownloadDiscount
 
 urlpatterns = [
-    path("swap_manager/", HomePageView.as_view(), name="home"),
+    path("swap-manager/", HomePageView.as_view(), name="home"),
     path(
-        "generate_chart/",
+        "generate-chart/",
         ChartGeneratorView.as_view(),
         name="generate_chart",
     ),
-    path("discount_chart/", DiscountChartView.as_view(), name="discount_chart"),
-    path("download_discount/", DownloadDiscount.as_view(), name="download_discount"),
+    path("discount-chart/", DiscountChartView.as_view(), name="discount_chart"),
+    path("download-discount/", DownloadDiscount.as_view(), name="download_discount"),
 ]
