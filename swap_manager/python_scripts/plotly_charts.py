9a39d205f4b4b993d8a4e7df1bb7949453402afb
Marcelo Barrero <marcelo.barrero@live.com>
2024-03-08 19:00:15 -0400

    swap_manager: plotly color advances

--- swap_manager/python_scripts/plotly_charts.py
+++ swap_manager/python_scripts/plotly_charts.py
@@ -7,10 +7,18 @@ def scatter_plot(x, y, mode="lines+markers"):
             x=x,
             y=y,
             mode=mode,
+            line=dict(color="#ff9c29", width=1),
+            marker=dict(color="#ff9c29", size=6),
         )
     ]
 
-    layout = go.Layout()
+    layout = go.Layout(
+        plot_bgcolor="#212121",
+        paper_bgcolor="black",
+        font=dict(color="white"),
+        xaxis=dict(color="white", gridcolor="#2e2e2e"),
+        yaxis=dict(color="white", gridcolor="#2e2e2e"),
+    )
     fig = go.Figure(data=data, layout=layout)
     chart = fig.to_html(full_html=False, include_plotlyjs=False)
 
