66d81fdda4aab1df89cb0c4dc80b292b8c6216ae
Marcelo Barrero <marcelo.barrero@live.com>
2024-04-23 19:35:56 -0400

    static root attempt

--- django_project/settings.py
+++ django_project/settings.py
@@ -128,6 +128,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static", BASE_DIR / "swap_manager" / "static"]
+STATIC_ROOT = "/home/marcelobarrero/marcelobarrero/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
