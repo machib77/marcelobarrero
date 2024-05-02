bba51bc65c97ded56b8527e3b3f4bcb3bfa96266
Marcelo Barrero <marcelo.barrero@live.com>
2024-05-02 15:26:28 -0400

    portfolio-optimization ticker functioning searchbar

--- portfolio_optimization/urls.py
+++ portfolio_optimization/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import HomePageView
+from . import views
 
 urlpatterns = [
     path("portfolio-optimization/", HomePageView.as_view(), name="po-home"),
 ]
+
+htmx_urlpatterns = [path("search-ticker/", views.search_ticker, name="search-ticker")]
+
+urlpatterns += htmx_urlpatterns
--- portfolio_optimization/views.py
+++ portfolio_optimization/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from portfolio_optimization.models import Ticker
+from django.db.models import Q
 
 
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = "portfolio_optimization/po-home.html"
+
+
+def search_ticker(request):
+    search_text = request.POST.get("search")
+
+    results = Ticker.objects.filter(
+        Q(symbol__icontains=search_text)
+        | Q(company_name__icontains=search_text)
+        | Q(index__icontains=search_text)
+    )
+    context = {"results": results}
+    return render(request, "partials/search-results.html", context)
