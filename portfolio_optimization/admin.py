b31268e668475e6678a4613b0bceac3f67115fa3
Marcelo Barrero <marcelo.barrero@live.com>
2024-05-02 14:40:27 -0400

    portfolio-optimization ticker database ready

--- portfolio_optimization/admin.py
+++ portfolio_optimization/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Ticker
+
 
 # Register your models here.
+class TickerAdmin(admin.ModelAdmin):
+    list_display = ("symbol", "company_name", "index")
+
+
+admin.site.register(Ticker, TickerAdmin)
--- portfolio_optimization/models.py
+++ portfolio_optimization/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Ticker(models.Model):
+    symbol = models.CharField(max_length=10)
+    company_name = models.CharField(max_length=100)
+    index = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.symbol
